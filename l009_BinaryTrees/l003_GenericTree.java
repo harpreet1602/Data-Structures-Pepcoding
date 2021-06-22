20b5c36e0ca4cb89ff6553f927cb756b9027025f
harpreet1602 <harpreets1602@gmail.com>
2021-06-23 00:01:07 +0530

    update

---
+++ l009_BinaryTrees/l003_GenericTree.java
@@ -0,0 +1,150 @@
+import java.util.ArrayList;
+public class l003_GenericTree {
+    public static class Node{
+        int data = 0;
+        ArrayList<Node> children;
+
+        Node(int data)
+        {
+            this.data=data;
+            this.children = new ArrayList<>();
+        }
+    }
+    //mere children ki maximum height mai +1 karke return kardo
+    //root sai 0 aana chaiye height (edge based height)
+  public static int height(Node node) {
+    // write your code here
+    int h=-1;
+    for(Node child:node.children)
+    {
+        h=Math.max(h,height(child));
+    }
+    return h+1;
+  }
+
+    //apne children ka size utha ke le aata hu aur khud ka
+    // +1 karke return kardeta hu 
+    public static int size1(Node node){
+        // write your code here
+        int count=0;
+        for(int i=0;i<node.children.size();i++){
+            Node child=node.children.get(i);
+            count+=size(child);
+        }
+        return count+1;
+      }
+      
+      public static int size(Node node){
+        int count = 0;
+        for(Node child:node.children)
+        {
+            count+=size(child);
+        }
+        return count+1;
+          
+      }
+    
+
+      public static int max(Node node) {
+        // write your code here
+        int m=node.data;
+        for(Node child:node.children)
+        {
+            m=Math.max(m,max(child));
+        }
+        return m;
+      }
+
+    public static int minimum(Node root)
+    {
+        int min=root.data;
+        for(int i=0;i<root.children.size();i++)
+        {
+            Node child=root.children.get(i);
+            min=Math.min(min,minimum(child));
+        }
+        return min;
+    }
+    public static int minmum(Node root)
+    {
+
+    }
+    //mai khud hu kya to return true nhi to children mai hai kya
+    //agar hai to vhi sai true hote rahega by using or ya phir if bhi 
+    //use kar sakte the
+    //or vala milne pai aage recursion nhi lagaega
+  public static boolean find(Node node, int data) {
+    // write your code here
+    if(node.data==data)
+    return true;
+    
+    boolean res=false;
+    for(Node child:node.children)
+    {
+        res=res||find(child,data);
+    }
+    return res;
+  }
+  //if vala milne pai break kardeta hai
+  public static boolean find1(Node node, int data) {
+  
+    if(node.data==data)
+    return true;
+    boolean res=false;
+    for(Node child:node.children)
+    {
+        if(find(child,data))
+        {
+            res=true;
+            break;
+        }
+    }
+    return res;
+}
+public static boolean nodeToRootPath(Node node,int data,ArrayList<Node> ans)
+{
+    if(node.data==data)
+    {
+        ans.add(node);
+        return true;
+    }
+    boolean res=false;
+    for(Node child:node.children)
+    {
+        res = res || nodeToRootPath(child, data, ans);
+    }
+    if(res)
+    {
+        ans.add(node);
+    } 
+    return res;
+}
+//mai hu data to nyi arraylist bana ke mujhe add karke return karva do root ki taraaf 
+//backtrack karte hue
+//ab hamesha nyi arraylist banti rahegi nhi hoga to empty hi return hoti rahegi
+//aur jab piche sai milni shuru hogyi to break karke loop bahar aake us node ke parent ko add kardenge
+//return kardenge list
+public static ArrayList<Integer> nodeToRootPath(Node node, int data){
+    // write your code here
+    if(node.data==data)
+    {
+        ArrayList<Integer> base= new ArrayList<>();
+        base.add(node.data);
+        return base;
+    }
+    
+    ArrayList<Integer> myAns=new ArrayList<>();
+    for(Node child:node.children)
+    {
+        myAns=nodeToRootPath(child,data);
+        if(myAns.size()!=0)
+        break;
+    }
+    if(myAns.size()!=0)
+    {
+        myAns.add(node.data);
+    }
+    return myAns;
+ }
+
+}
---
+++ l009_BinaryTrees/questionsGenericTree.java
@@ -0,0 +1,36 @@
+public class questionsGenericTree {
+    
+
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+    
+public int maxDepth_(Node root) {
+       
+    int height=0;
+    for(Node child:root.children)
+    {
+        height=Math.max(height,maxDepth_(child));
+    }
+    return height+1;
+}
+
+public int maxDepth(Node root){
+ if(root==null)
+        return 0;
+   return  maxDepth_(root);
+}
+}
