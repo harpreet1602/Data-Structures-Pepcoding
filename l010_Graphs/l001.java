a0ebf162f1fbe66e1b6667de8f31125be3faab65
harpreet1602 <harpreets1602@gmail.com>
2021-06-29 13:44:48 +0530

    update

--- l010_Graphs/l001.java
+++ l010_Graphs/l001.java
@@ -20,15 +20,67 @@ public class l001
     }
     public static void display(ArrayList<Edge>[] graph,int N)
     {
-        
+        for(int i=0;i<N;i++){
+            System.out.print(i+" -> ");
+            // for(int j=0;j<graph[i].size();i++)
+            // {
+            //     System.out.println("("+graph[i].get(j).nbr+" , "+graph[i].get(j).wt+")");
+            // }
+                //2nd way
+             for(Edge e:graph[i]){
+                    System.out.println("(" + e.nbr + "," + e.wt + ")");
+             }
+            System.out.println();
+        }
     }
-    
-    public static void removeEdge(int u,int v){
-
-    }
-    public static void removeVtx(int u)
+    public static int findIndex(ArrayList<Edge>[] graph,int u,int v)
     {
+        ArrayList<Edge> list=graph[u];
+        for(int i=0;i<list.size();i++)
+        {
+            Edge e=list.get(i);
+            if(e.nbr==v)
+            return i;
+        }
+        return -1;
+    }
+    public static void removeEdge(ArrayList<Edge>[] graph,int u,int v){
+        int i1=findIndex(graph,u,v);
+        int i2=findIndex(graph,v,u);
 
+        graph[u].remove(i1);         //u sai i1 edge remove 
+        graph[v].remove(i2);         //v sai i2 edge remove  
+    }
+    public static void removeVtx(ArrayList<Edge>[] graph,int u)
+    {
+        ArrayList<Edge> list = graph[u];
+        for(int i=list.size()-1;i>=0;i--)
+        {
+            Edge e=list.get(i);
+            removeEdge(graph,e.src,e.nbr);
+        } 
+    }
+    //dfs
+    //1.mark
+    //2.visit all the unmarked nodes
+    //2.1 call dfs
+    public static boolean hasPath(ArrayList<Edge>[] graph,int u,int v,boolean[] vis)
+    {
+        if(u==v)
+        return true;
+        
+        boolean res=false;
+        vis[u]=true;
+        
+        for(Edge e:graph[u])
+        {
+            if(!vis[e.nbr])
+            {
+                res=res||hasPath(graph,e.nbr,v,vis);
+            }
+        }
+        return res;
+        
     }
 
     public static void construction(){
