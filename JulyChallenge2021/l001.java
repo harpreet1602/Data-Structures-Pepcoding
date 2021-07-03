31710b8a466329125f01081dd252808137c36544
harpreet1602 <harpreets1602@gmail.com>
2021-07-03 13:38:54 +0530

    update

--- JulyChallenge2021/l001.java
+++ JulyChallenge2021/l001.java
@@ -21,4 +21,51 @@ public class l001{
        }
        return curr;
    }
+
+
+   //leetcode 658. Find K Closest Elements
+
+   //O(n)
+   public List<Integer> findClosestElements1(int[] arr, int k, int x) {
+    int low=0,n=arr.length,high=n-1;
+       while(high-low>=k)
+       {
+           if(Math.abs(x-arr[low]) > Math.abs(x-arr[high]))
+               low++;
+           else
+               high--;
+       }
+        List<Integer> ans = new ArrayList<Integer>();
+        for(int i=low ; i<=high; i++ )
+        {
+            ans.add(arr[i]);
+        }
+        return ans;
+    
+    }
+
+
+
+    //O(log(N-k)+k)
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left=0,right=arr.length-k;
+        while(left<right)            //O(log(N-k))
+         {
+            int mid=(left+right)/2;
+            if(x-arr[mid]>arr[mid+k]-x)
+            {
+                left=mid+1;
+                
+            }
+            else
+                right=mid;
+        }
+        List<Integer> ans=new ArrayList<Integer>();
+        for(int i=left;i<left+k;i++)             //O(k)
+        {
+            ans.add(arr[i]);
+        }
+        return ans;   
+    }
+
 }
