39e749493ad760e3c1a6704782d65469226bbe27
harpreet1602 <harpreets1602@gmail.com>
2021-07-11 23:10:05 +0530

    update

--- JulyChallenge2021/l001.java
+++ JulyChallenge2021/l001.java
@@ -489,4 +489,39 @@ O(N2)
         return omax;
     }
 
+    //91. Decode Ways
+    public int numDecodings(String str) {
+        int n=str.length();
+        int[] dp = new int[n];
+        if(n>0 && str.charAt(0)=='0')
+            return 0;
+        dp[0] = 1;
+        for(int i = 1; i<n ;i++)
+        {
+            if(str.charAt(i-1)=='0' && str.charAt(i)=='0')
+                   dp[i] = 0;
+            else if(str.charAt(i-1)=='0' && str.charAt(i)!='0'){
+                    dp[i] = dp[i-1];        
+                }
+            else if(str.charAt(i-1)!='0' && str.charAt(i)=='0'){
+                if(str.charAt(i-1)=='1' || str.charAt(i-1)=='2'){    
+                dp[i] = (i >=2)? dp[i-2] : 1;
+                }
+                }
+            else {
+                    if(Integer.parseInt(str.substring(i-1,i+1)) <= 26 )
+                    {
+                        dp[i] = dp[i-1] + ((i >=2)? dp[i-2] : 1);   
+                    }
+                    else
+                    {
+                        dp[i] = dp[i-1];
+                    }
+                }
+        }
+        return dp[n-1];
+    }
+
+    
+
 }
