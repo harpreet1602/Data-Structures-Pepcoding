8a57eb6aa7eba9e624160d72037fe865122ebf89
harpreet1602 <harpreets1602@gmail.com>
2021-07-14 01:54:12 +0530

    updATE

--- JulyChallenge2021/l001.java
+++ JulyChallenge2021/l001.java
@@ -637,6 +637,34 @@ O(N2)
             return max;
         }
     
+        //optimized
+            //time:O(log(n)) space: O(1)
+     public int findPeakElementOptimized(int[] nums) {
+        int low=0,high=nums.length,max=0,left=0,right=0;
+         while(low<=high)
+         {
+            int mid=(low+high)/2;
+            left=mid>0?nums[mid-1]:Integer.MIN_VALUE;
+            right=mid<nums.length-1?nums[mid+1]:Integer.MIN_VALUE;
+            if(nums[mid]>left && nums[mid]>right)
+            {
+                max=mid;
+                break;
+            }
+            else if(nums[mid]<=left)
+            {
+                high=mid-1;
+            }
+             else if(nums[mid]<=right)
+             {
+                low=mid+1;
+             }
+         }
+         return max;
+     }
+
+
+
     //[1,2,3,1]
     // [1,2,1,3,5,6,4]
     // [-2147483648]
