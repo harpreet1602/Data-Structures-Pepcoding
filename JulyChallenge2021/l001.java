5cd8c7f5a6a81570cc052739c45ac8c6b3a2e2c5
harpreet1602 <harpreets1602@gmail.com>
2021-07-17 16:07:54 +0530

    update

--- JulyChallenge2021/l001.java
+++ JulyChallenge2021/l001.java
@@ -719,4 +719,55 @@ public int triangleNumber1(int[] nums) {
     //
     // [-2147483648,-2147483647]
 
+
+     public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0; i<nums.length-3;i++) 
+        {
+            if(i!=0 && nums[i]==nums[i-1])
+                continue;
+            for(int j = i+1; j<nums.length-2;j++)
+            {
+                if(j!=i+1 && nums[j]==nums[j-1])
+                    continue;
+                int left = j+1,right=nums.length-1;
+                while(left<right)
+                {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum == target)
+                    {
+                        ArrayList<Integer> small = new ArrayList<>();
+                        small.add(nums[i]);
+                        small.add(nums[j]);
+                        small.add(nums[left]);
+                        small.add(nums[right]);
+                        list.add(small);
+                        
+                        left++;
+                        right--;
+                        
+                        while(left<right && nums[left]==nums[left-1])
+                        {
+                            left++;
+                        }
+                        while(left<right && nums[right] == nums[right+1])
+                        {
+                            right--;
+                        }
+                    }
+                    else if(sum < target)
+                    {
+                           left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+        }
+        return list;
+    }
+
 }
