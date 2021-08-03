98b7858aa5505992b7eeb2bd2fc983f3f9443609
harpreet1602 <harpreets1602@gmail.com>
2021-08-04 02:05:54 +0530

    up

--- Challenges/AugustChallenge2021/l001.java
+++ Challenges/AugustChallenge2021/l001.java
@@ -1,4 +1,7 @@
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 public class l001{
     // 1. Two Sum
 //     So in this brute force will be fixing one and checking with other TC O(n^2) SC O(1)
@@ -19,6 +22,41 @@ public int[] twoSum(int[] nums, int target) {
     }
     return ans;
 }
+
+
+
+// Sort the given array then use the concept of previous if the current ele = prev then 
+// call will not be made for the duplicate element
+// Rest of the question is then finding the subsets using this combination method
+public int subsetsDup(int[] nums,int idx,List<Integer> smallAns, List<List<Integer>> ans)
+    {
+        List<Integer> base = new ArrayList<>(smallAns);
+        ans.add(base);
+        int count=0,prev=-11;
+        for(int i =idx;i<nums.length;i++)
+        {
+            if(prev!=nums[i])
+            {
+            smallAns.add(nums[i]);    
+            count+=subsetsDup(nums,i+1,smallAns,ans);
+            smallAns.remove(smallAns.size()-1);
+            }
+            prev=nums[i];
+        }
+        
+        
+        return count;
+        
+    }
+    
+    
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<Integer> smallAns = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        subsetsDup(nums,0,smallAns,ans);
+        return ans;
+    }
 
 
 
