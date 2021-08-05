9672c5e363e0a71617c713135a5585b41c07746c
harpreet1602 <harpreets1602@gmail.com>
2021-08-05 13:46:49 +0530

    up

--- LevelUp/l001_Recursion/l004_questions.java
+++ LevelUp/l001_Recursion/l004_questions.java
@@ -2,6 +2,20 @@ import java.util.*;
 
 public class l004_questions {
 
+    // 37. Sudoku Solver
+    // In this question for solving the sudoku we are storing the spaces in an ArrayList of Pair
+    // then we are traversing through these empty spaces and checking is it valid to place a number there
+    // if it then we place the number and go to the next index for placing a number from 1-9
+    // and then in backtracking place the . again to figure out the next possibility if we don't get the answer
+    // in the end return false but if we got the answer then start returning true
+    // when will we get the answer when all the empty spaces  will be filled by the numebers
+    // in is valid to place function we will check for the same no. in row,col,submatrix if it is present already 
+    // return false otherwise return true;
+    
+    // in the submatrix 
+    
+        // imp to figure out this , here the remainder will be finished by dividing and then by multiplying 
+        // we can get the starting indices of the submatrix (5,3) => (5/3,3/3) => (1,1) =>(1*3,1*3) =>(3,3)
     public class Pair{
         int r;
         int c;
@@ -11,7 +25,6 @@ public class l004_questions {
             this.c = c;
         }
     }
-    
     public boolean isValidToPlaceNumber(char[][] board,int r,int c,int num){
 //         row
         for(int i = 0 ; i < board[0].length ; i++ )
@@ -33,7 +46,8 @@ public class l004_questions {
         
 //         submatrix of 3X3
 
-        
+        // imp to figure out this , here the remainder will be finished by dividing and then by multiplying 
+        // we can get the starting indices of the submatrix
         int row = (r/3) * 3;
         int col = (c/3) * 3;
         
