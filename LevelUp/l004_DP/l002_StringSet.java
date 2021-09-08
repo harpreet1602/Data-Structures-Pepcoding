e8e4e481098f1469aa0d235db20a2d390468d6e9
harpreet1602 <harpreets1602@gmail.com>
2021-09-09 00:11:18 +0530

    update

--- LevelUp/l004_DP/l002_StringSet.java
+++ LevelUp/l004_DP/l002_StringSet.java
@@ -117,6 +117,30 @@ public class l002_StringSet {
         }
         return dp[n][m];
     }
+       
+    public int minDistanceTab(String word1, String word2, int N, int M, int[][] dp){
+        for(int n = 0;n<=N;n++){
+            for(int m=0;m<=M;m++){
+                
+        if(n==0 || m==0){
+            dp[n][m] = n==0?m:n;
+            continue;
+        }
+        
+        int insert = dp[n][m-1];
+        int delete = dp[n-1][m];
+        int replace = dp[n-1][m-1];
+        
+        if(word1.charAt(n-1) == word2.charAt(m-1)){
+            dp[n][m] = replace;
+        }
+        else{
+            dp[n][m] = Math.min(insert,Math.min(delete,replace)) + 1;
+        }
+            }
+        }
+        return dp[N][M];
+    }
     public int minDistance(String word1, String word2) {
         int n = word1.length(), m = word2.length();
         int[][] dp =new int[n+1][m+1];
