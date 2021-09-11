b53b86b70a6907baf016b0756f2f52c9139b76b8
harpreet1602 <harpreets1602@gmail.com>
2021-09-11 20:47:46 +0530

    update

--- LevelUp/l004_DP/l002_StringSet.java
+++ LevelUp/l004_DP/l002_StringSet.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 public class l002_StringSet {
     
 // pending dp
@@ -141,14 +143,125 @@ public class l002_StringSet {
         }
         return dp[N][M];
     }
-    public int minDistance(String word1, String word2) {
+
+    // follow up question
+    // 1. IF THE COST OF INSERT DELETE AND REPLACE IS GIVEN THEN FIND THE MINIMUM COST FOR THE 
+    // OPERATION OF CONVERTING THE STR1 TO STR2
+
+    public static int minDistanceFollow(String word1, String word2, int n, int m, int[] cost, int[][] dp){
+        if(n==0 || m==0){
+            return n==0?m*cost[0]:n*cost[2];
+        }
+        
+        if(dp[n][m]!=-1)
+            return dp[n][m];
+        
+        int insert = minDistanceFollow(word1,word2,n,m-1,cost,dp);
+        int delete = minDistanceFollow(word1,word2,n-1,m,cost,dp);
+        int replace = minDistanceFollow(word1,word2,n-1,m-1,cost,dp);
+        
+        if(word1.charAt(n-1) == word2.charAt(m-1)){
+            dp[n][m] = replace;
+        }
+        else{
+            dp[n][m] = Math.min(insert + cost[0],Math.min(delete + cost[2],replace + cost[1]));
+        }
+        return dp[n][m];
+    }
+
+    public static int minDistance(String word1, String word2) {
         int n = word1.length(), m = word2.length();
         int[][] dp =new int[n+1][m+1];
         for(int[] d:dp){
             Arrays.fill(d,-1);
         }
-        return minDistanceMemo(word1,word2,n,m,dp);
+        int[] cost = {4,5,7};
+        // return minDistanceMemo(word1,word2,n,m,dp);
+        return minDistanceFollow(word1, word2, n, m, cost, dp);
+    }
+    public static void main(String[] args){
+
+        System.out.println(minDistance("saturday","sunday"));        
     }
 
+
+    // 1035. Uncrossed Lines
+    public int maxUncrossedLines(int[] nums1, int[] nums2, int n, int m,int[][] dp){
+        if(n==0 || m==0){
+            return dp[n][m] = 0;
+        }
+        if(dp[n][m]!=-1){
+            return dp[n][m];
+        }
+        if(nums1[n-1] == nums2[m-1]){
+            dp[n][m] = maxUncrossedLines(nums1,nums2,n-1,m-1,dp) + 1;
+        }
+        else{
+            dp[n][m] = Math.max(maxUncrossedLines(nums1,nums2,n-1,m,dp),maxUncrossedLines(nums1,nums2,n,m-1,dp));
+            
+        }
+        return dp[n][m];
+    }
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int n = nums1.length, m = nums2.length;
+        int[][] dp = new int[n+1][m+1];
+        for(int[] d:dp){
+            Arrays.fill(d,-1);
+        }
+        int ans = maxUncrossedLines(nums1, nums2, n, m, dp);
+        return ans;
+    }
+
+    // 1458. Max Dot Product of Two Subsequences
+    public int maximum(int... arr){
+        int max = arr[0];
+        for(int ele:arr){
+            max = Math.max(max,ele);
+        }
+        return max;
+    }
+    public int maxDotProduct(int[] nums1, int[] nums2, int n, int m, int[][] dp){
+        if(n==0 || m==0){
+            return dp[n][m]=-(int)1e8;
+        }
+        if(dp[n][m]!=-(int)1e9){
+            return dp[n][m];
+        }
+        int val = nums1[n-1]*nums2[m-1];
+        int bothValAccepted = maxDotProduct(nums1,nums2,n-1,m-1,dp) + val;
+        int a = maxDotProduct(nums1,nums2,n-1,m,dp);
+        int b = maxDotProduct(nums1,nums2,n,m-1,dp);
+        return dp[n][m] = maximum(val,bothValAccepted,a,b);
+    }
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        int n = nums1.length, m = nums2.length;
+        int[][] dp = new int[n+1][m+1];
+        for(int[] d:dp){
+            Arrays.fill(d,-(int)1e9);
+        }
+        int ans = maxDotProduct(nums1, nums2, n, m, dp);
+        return ans;
+    }
+    // 139. Word Break
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>();
+        int len = 0, n = s.length();
+        for(String ss : wordDict){
+            set.add(ss);
+            len = Math.max(ss.length(),len);
+        }
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+        for(int i = 0;i<=n;i++){
+            if(!dp[i]) continue;
+            for(int l=1;l<=len && i+l<=n;l++){
+                String substr = s.substring(i,i+l);
+                if(set.contains(substr)){
+                    dp[i+l] = true;
+                }
+            }
+        }
+        return dp[n];
+    }
     
 }
