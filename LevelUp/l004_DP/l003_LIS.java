bc787a3423c53a8f73cee832a1e7450f9ae3419d
harpreet1602 <harpreets1602@gmail.com>
2021-09-12 22:19:37 +0530

    update

--- LevelUp/l004_DP/l003_LIS.java
+++ LevelUp/l004_DP/l003_LIS.java
@@ -14,7 +14,7 @@ public class l003_LIS {
         }
         return dp[ei] = maxLen;
     }
-      public int LIStab(int[] nums, int[] dp){
+      public int LIS_LR(int[] nums, int[] dp){
        int n = nums.length,maxLen=0;
         for(int i = 0;i<n;i++){
           
@@ -28,6 +28,20 @@ public class l003_LIS {
         }
           return maxLen;
     }
+    public int LIS_RL(int[] nums, int[] dp){
+        int n = nums.length,maxLen=0;
+         for(int i = n-1;i>=0;i--){
+           
+         dp[i]=1;
+         for(int j =i+1;j<n;j++){
+             if(nums[j]<nums[i]){
+             dp[i] = Math.max(dp[i],dp[j]+1);
+             }
+         }
+         maxLen = Math.max(dp[i],maxLen);
+         }
+           return maxLen;
+     }
     public int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
 //         int maxLen = 0;
@@ -37,10 +51,10 @@ public class l003_LIS {
            
 //         }
 //         return maxLen;
-        return LIStab(nums,dp);
+        return LIS_LR(nums,dp);
     }
 
-    public int LDStab(int[] nums, int[] dp){
+    public int LDS_LR(int[] nums, int[] dp){
         int n = nums.length,maxLen=0;
          for(int i = 0;i<n;i++){
            
@@ -54,4 +68,37 @@ public class l003_LIS {
          }
            return maxLen;
      }
+
+     public int LDS_RL(int[] nums, int[] dp){
+        int n = nums.length,maxLen=0;
+         for(int i = n-1;i>=0;i--){
+           
+         dp[i]=1;
+         for(int j =i+1;j<n;j++){
+             if(nums[j]>nums[i]){
+             dp[i] = Math.max(dp[i],dp[j]+1);
+             }
+         }
+         maxLen = Math.max(dp[i],maxLen);
+         }
+           return maxLen;
+     }
+
+// https://practice.geeksforgeeks.org/problems/longest-bitonic-subsequence0824/1
+     public int LongestBitonicSequence(int[] nums)
+     {
+         // Code here
+         int  n =nums.length;
+         int[] LIS = new int[n];
+         int[] LDS = new int[n];
+         LIS_LR(nums,LIS);
+         LIS_RL(nums,LDS);
+         int maxLen=0;
+         for(int i =0 ;i < n ; i++){
+             maxLen = Math.max(maxLen, LIS[i] + LDS[i]-1);
+             
+         }
+         return maxLen;
+     }
+
 }
