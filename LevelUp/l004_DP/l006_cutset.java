ad7961bdd0d3bd5957af95e0dd671896f772fd86
harpreet1602 <harpreets1602@gmail.com>
2021-09-24 02:27:39 +0530

    update

--- LevelUp/l004_DP/l006_cutset.java
+++ LevelUp/l004_DP/l006_cutset.java
@@ -43,7 +43,11 @@ public class l006_cutset {
     public static int mcm_tab(int[] arr,int Si,int Ei,int[][] dp){
         return dp[Si][Ei];
     }   
+
     // 312. Burst Balloons
+// cut the array on a index take the value from left and right you will get the cost then for
+// the current index find the cost by multiplying its left val and right val with the current val 
+// and add all the cost from leftcost + current + right cost  
     public int maxCoins(int[] nums, int si,int ei,int[][] dp) {
         if(dp[si][ei]!=0) return dp[si][ei];
         int lele = si==0?1:nums[si-1];
@@ -62,6 +66,95 @@ public class l006_cutset {
         int[][] dp = new int[n][n];
         return maxCoins(nums, 0,n-1,dp);
     }
+
+    // https://practice.geeksforgeeks.org/problems/boolean-parenthesization5610/1
+    static class pairBoolean{
+        long totaltrue=0;
+        long totalfalse=0;
+        pairBoolean(){
+            
+        }
+        pairBoolean(int totaltrue,int totalfalse){
+            this.totaltrue = totaltrue;
+            this.totalfalse = totalfalse;
+        }
+    }
+    static void evaluate(pairBoolean leftres,pairBoolean rightres,pairBoolean res,char operator){
+        long mod = 1003;
+        long totaltf = (leftres.totaltrue+leftres.totalfalse) * (rightres.totaltrue+rightres.totalfalse);
+        long totaltrue = 0, totalfalse = 0;
+        
+        
+        if(operator == '|'){
+            totalfalse = (leftres.totalfalse * rightres.totalfalse) % mod;
+            totaltrue = (totaltf - totalfalse + mod) % mod;
+        }
+        else if(operator == '&'){
+            totaltrue = (leftres.totaltrue * rightres.totaltrue) % mod;
+            totalfalse = (totaltf - totaltrue + mod) % mod;
+        }
+        else if(operator == '^'){
+            totaltrue = ((leftres.totaltrue * rightres.totalfalse) + (leftres.totalfalse * rightres.totaltrue) ) % mod;
+            totalfalse = (totaltf - totaltrue + mod) % mod;
+        }
+        
+        res.totaltrue = (res.totaltrue + totaltrue) % mod;
+        
+        res.totalfalse = (res.totalfalse + totalfalse) % mod;
+
+    }
+    static pairBoolean countWays(String s, int si, int ei, pairBoolean[][] dp){
+        if(si==ei){
+            char ch = s.charAt(si);
+            int t = ch == 'T'?1:0;
+            int f = ch == 'F'?1:0;
+            return dp[si][ei] = new pairBoolean(t,f);
+        }
+        if(dp[si][ei]!=null){
+            return dp[si][ei];
+        }
+        pairBoolean res = new pairBoolean();
+        // operators pai cut lage aur left right mai expression jaye
+        
+        for(int cut=si+1;cut<ei;cut+=2){
+            pairBoolean leftres = countWays(s,si,cut-1,dp);
+            pairBoolean rightres = countWays(s,cut+1,ei,dp);
+            
+            evaluate(leftres,rightres,res,s.charAt(cut));
+        }
+        return dp[si][ei] = res;
+    }
+    
+    static int countWays(int N, String S){
+        // code here
+        
+        pairBoolean[][] dp = new pairBoolean[N][N];
+        return (int)countWays(S,0,N-1,dp).totaltrue;
+    }
+
+// https://www.geeksforgeeks.org/optimal-binary-search-tree-dp-24/
+    // https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/dynamic-programming/optimal-bst-official/ojquestion
+    public static int optimalbst(int[] keys,int[] frequency,int si,int ei,int[][] dp){
+        if(dp[si][ei]!=0){
+            return dp[si][ei];
+        }
+        int mincost = (int)1e9;
+        int sum=0;
+        for(int cut = si; cut<=ei; cut++){
+            int leftcost = si==cut?0:optimalbst(keys,frequency,si,cut-1,dp);
+            
+            int rightcost = ei==cut?0:optimalbst(keys,frequency,cut+1,ei,dp);
+            mincost = Math.min(mincost,leftcost+(0)+rightcost);
+            sum+=frequency[cut];
+        }
+        return dp[si][ei] = mincost + sum;
+    }
+    private static void optimalbst(int[] keys, int[] frequency, int n) {
+      //write your code here
+      int[][] dp =new int[n][n];
+      System.out.println(optimalbst(keys,frequency,0,n-1,dp));
+      }
+  
     public static void main(String[] args){
         mcm();
     }
