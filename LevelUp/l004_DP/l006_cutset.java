1a996c749c8f956f5834cd61bd4d2f7f4b6e9a67
harpreet1602 <harpreets1602@gmail.com>
2021-09-23 19:36:54 +0530

    update

--- LevelUp/l004_DP/l006_cutset.java
+++ LevelUp/l004_DP/l006_cutset.java
@@ -43,8 +43,25 @@ public class l006_cutset {
     public static int mcm_tab(int[] arr,int Si,int Ei,int[][] dp){
         return dp[Si][Ei];
     }   
-
-
+    // 312. Burst Balloons
+    public int maxCoins(int[] nums, int si,int ei,int[][] dp) {
+        if(dp[si][ei]!=0) return dp[si][ei];
+        int lele = si==0?1:nums[si-1];
+        int rele = ei==nums.length-1?1:nums[ei+1];
+        int maxCoins=0;
+        for(int cut = si;cut<=ei;cut++){
+            int leftcost =  cut==si?0:maxCoins(nums,si,cut-1,dp);
+            int rightcost = cut==ei?0:maxCoins(nums,cut+1,ei,dp);
+            
+            maxCoins = Math.max(maxCoins,leftcost + lele*nums[cut]*rele + rightcost);
+        }
+        return dp[si][ei] = maxCoins;
+    }
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+        return maxCoins(nums, 0,n-1,dp);
+    }
     public static void main(String[] args){
         mcm();
     }
