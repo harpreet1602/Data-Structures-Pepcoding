cdd2af6fa0fbae119dff98ca9abdab51914ea40c
harpreet1602 <harpreets1602@gmail.com>
2021-10-12 23:52:27 +0530

    update

--- LevelUp/l005_StacksAndQueues/questions/l001.java
+++ LevelUp/l005_StacksAndQueues/questions/l001.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.ArrayList;
 public class l001 {
     
@@ -318,5 +320,187 @@ public int[] asteroidCollision(int[] asteroids) {
             }
             return sb.toString();
         }
+        // 895. Maximum Frequency Stack
+        // 1
+        class FreqStack {
+        private class pair implements Comparable<pair>{
+            int val = 0;
+            int idx = 0;
+            int freq = 0;
 
+            pair(int val,int idx,int freq){
+                this.val = val;
+                this.idx = idx;
+                this.freq = freq;
+            }
+
+            public int compareTo(pair o){
+                if(this.freq == o.freq){
+                    return o.idx-this.idx;
+                } 
+                else{
+                    return o.freq-this.freq;
+                }
+            }
+        } 
+        private PriorityQueue<pair> pq;
+        private HashMap<Integer,Integer> freq;
+        private int idx = 0;
+
+        public FreqStack() {
+            pq = new PriorityQueue<>();
+            freq = new HashMap<>();    
+        }
+
+        public void push(int val) {
+            freq.put(val,freq.getOrDefault(val, 0)+1);
+            pq.add(new pair(val,idx++,freq.get(val)));    
+        }
+        
+        public int pop() {
+            pair rp = pq.remove();
+            freq.put(rp.val,rp.freq-1);
+            if(freq.get(rp.val) == 0){
+                freq.remove(rp.val);
+            }
+            
+            return rp.val;
+        }
+
+        public int top()
+        {
+            pair rp = pq.peek();
+            return rp.val;
+        }
+    }
+
+// 2
+        ArrayList<LinkedList<Integer>> freqMap;
+        HashMap<Integer,Integer> map;
+        int maxFreq = 0;
+        public FreqStack() {
+               freqMap = new ArrayList<>();
+               map = new HashMap<>();
+               freqMap.add(new LinkedList<>());
+        }
+
+        public void push(int val) {
+            map.put(val,map.getOrDefault(val,0)+1);
+            maxFreq = Math.max(maxFreq,map.get(val));
+            if(maxFreq == freqMap.size()){
+                freqMap.add(new LinkedList<>());
+            }
+            freqMap.get(map.get(val)).addFirst(val);
+        }
+        
+        public int pop() {
+            int rv = freqMap.get(maxFreq).removeFirst();
+            
+            if(freqMap.get(maxFreq).size() == 0){
+                freqMap.remove(maxFreq--);
+            }
+            map.put(rv,map.get(rv)-1);
+            if(map.get(rv) == 0){
+                map.remove(rv);
+            }
+            return rv;
+        }
+          
+        //3 
+
+        
+       ArrayList<ArrayDeque<Integer>> freqMap;
+       HashMap<Integer,Integer> map;
+       int maxFreq = 0;
+       public FreqStack() {
+              freqMap = new ArrayList<>();
+              map = new HashMap<>();
+              freqMap.add(new ArrayDeque<>());
+       }
+
+       public void push(int val) {
+           map.put(val,map.getOrDefault(val,0)+1);
+           maxFreq = Math.max(maxFreq,map.get(val));
+           if(maxFreq == freqMap.size()){
+               freqMap.add(new ArrayDeque<>());
+           }
+           freqMap.get(map.get(val)).addFirst(val);
+       }
+       
+       public int pop() {
+           int rv = freqMap.get(maxFreq).removeFirst();
+           
+           if(freqMap.get(maxFreq).size() == 0){
+               freqMap.remove(maxFreq--);
+           }
+           map.put(rv,map.get(rv)-1);
+           if(map.get(rv) == 0){
+               map.remove(rv);
+           }
+           return rv;
+       }
+    }
+    
+
+    
+    // 155. Min Stack
+    class MinStack {
+
+        LinkedList<Long> st;
+        long msf = 0;
+        public MinStack() {
+            st = new LinkedList<>();
+        }
+        
+        public void push(int val) {
+            long x =val;
+            if(st.size() == 0){
+                st.addFirst((Long)x);
+                msf = x;
+            }
+            else{
+                if(x<msf){
+                    st.addFirst(2*x - msf);
+                    msf = x;
+                }
+                else{
+                    st.addFirst((Long)x);
+                }
+            }
+        }
+        
+        public void pop() {
+            long x = (long)st.removeFirst();
+            if(x<msf){
+                msf = 2*msf - x;
+            }
+        }
+        
+        public int top() {
+            long x = (long)st.getFirst();
+            if(x<msf){
+                return (int)msf;
+            }
+            return (int)(long)st.getFirst();
+        }
+        
+        public int getMin() {
+            return (int)msf;
+        }
+ /**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+        // 636. Exclusive Time of Functions
+        public int[] exclusiveTime(int n, List<String> logs) {
+            class logpair{
+                
+            }       
+        }
+    
 }
