c973755b165428cdfe3d1c4904bc266b1de13ccc
Harpreet Singh <harpreets1602@gmail.com>
2021-10-22 01:58:34 +0530

    update

--- Challenges/OctoberChallenge2021/l001.java
+++ Challenges/OctoberChallenge2021/l001.java
@@ -373,7 +373,51 @@ public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 	return ans;
 }
 
+// 380. Insert Delete GetRandom O(1)
+class RandomizedSet {
+    HashMap<Integer,Integer> map;
+    List<Integer> list;
+    Random r;
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        r = new Random();
+    }
+    
+    public boolean insert(int val) {
+        if(map.containsKey(val))
+            return false;
+        list.add(val);
+        map.put(val,list.size()-1);
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if(!map.containsKey(val))
+            return false;
+        int ridx = map.get(val);
+        if(ridx!=list.size()-1){
+            list.set(ridx,list.get(list.size()-1));
+            map.put(list.get(ridx),ridx);
+        }
+        list.remove(list.size()-1);    
+        map.remove(val);
+        return true;
+    }
+    
+    public int getRandom() {
+        int randomIdx = r.nextInt(list.size());
+        return list.get(randomIdx);
+    }
+}
 
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
 
 	public static void main (String[] args) throws java.lang.Exception
 	{
