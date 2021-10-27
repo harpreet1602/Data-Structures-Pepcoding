e143a78b1ab01912e2f37433a3f835bbb45693ed
Harpreet Singh <harpreets1602@gmail.com>
2021-10-28 01:05:17 +0530

    update

--- LevelUp/l007_SearchingSorting/l001.java
+++ LevelUp/l007_SearchingSorting/l001.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 public class l001{
 
     public int binarySearch(int[] arr,int si,int ei,int data){
@@ -103,6 +105,87 @@ public class l001{
         return si;
     }
 
+    // https://practice.geeksforgeeks.org/problems/inversion-of-array-1587115620/1#
+     // arr[]: Input Array
+    // N : Size of the Array arr[]
+    //Function to count inversions in the array.
+    static long totalInversion(long[] arr,long[] sortedArray,int si,int mid, int ei){
+        int i = si, j = mid+1, k = si;
+        long count=0;
+        while(i<=mid && j<=ei){
+            if(arr[i] <= arr[j]){
+                sortedArray[k++] = arr[i++];
+            }
+            else{
+                sortedArray[k++] = arr[j++];
+                count+=mid-i+1;
+            }
+        }
+        while(i<=mid || j<=ei){
+            sortedArray[k++] = i<=mid?arr[i++]:arr[j++];
+        }
+        while(si<=ei){
+            arr[si] = sortedArray[si++];
+        }
+        return count;
+    }
+    
+    static long inversionCount(long[] arr,long[] sortedArray,int si,int ei){
+        if(si>=ei){
+            return 0;
+        }
+        int mid = (si+ei)/2;
+        long count=0;
+        count+=inversionCount(arr,sortedArray,si,mid);
+        count+=inversionCount(arr,sortedArray,mid+1,ei);
+        
+        count += totalInversion(arr,sortedArray,si,mid,ei);
+        
+        return count;
+    }
+    static long inversionCount(long arr[], long N)
+    {
+        // Your Code Here
+        if(N==0){
+            return 0;
+        }
+        long[] sortedArray = new long[(int)N];
+        return inversionCount(arr,sortedArray,0,(int)N-1);
+    }
+
+    // 658. Find K Closest Elements
+    
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int n = arr.length;
+        List<Integer> list = new ArrayList<>();
+        if(x<arr[0]){
+            for(int i=0;i<k;i++){
+                list.add(arr[i]);
+            }
+        }
+        else if(x>arr[n-1]){
+            for(int i=n-k;i<n;i++){
+                list.add(arr[i]);
+            }
+        }
+        else{
+            int idx = preferedLocation(arr,x);
+            int si = Math.max(0,idx-k);
+            int ei = Math.min(n-1,idx+k);
+            while(ei-si+1>k){
+                if(x-arr[si] <= arr[ei]-x){
+                    ei--;
+                }
+                else{
+                    si++;
+                }
+            }
+            for(int i=si;i<=ei;i++){
+                list.add(arr[i]);
+            }
+        }
+        return list;
+    }
     public static void main(String[] args){
 
     }
