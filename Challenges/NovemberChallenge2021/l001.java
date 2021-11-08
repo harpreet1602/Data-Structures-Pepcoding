34295d59c4e68d2befed0c79058f0c3ca84c0b36
Harpreet Singh <harpreets1602@gmail.com>
2021-11-09 03:13:11 +0530

    update

--- Challenges/NovemberChallenge2021/l001.java
+++ Challenges/NovemberChallenge2021/l001.java
@@ -440,6 +440,25 @@ public int uniquePathsIII(int[][] grid) {
         }
         return sb.toString();
     }
+
+    // 96. Unique Binary Search Trees
+     public int numTrees(int n) {
+        int[] dp = new int[n+1];
+        for(int i=0;i<=n;i++){
+            if(i<=1){
+                dp[i] = 1;
+                continue;
+            }
+            int l = 0;
+            int r = i-1;
+            while(l<=i-1){
+                dp[i] += dp[l]*dp[r];
+                l++;
+                r--;
+            }
+        }
+        return dp[n];
+    }
    public static void main(String[] args){
 
    }
