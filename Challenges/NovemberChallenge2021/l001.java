e5b77b05574afc42ed37d921d9a27484e451ae55
Harpreet Singh <harpreets1602@gmail.com>
2021-11-26 13:41:28 +0530

    update

--- Challenges/NovemberChallenge2021/l001.java
+++ Challenges/NovemberChallenge2021/l001.java
@@ -1401,7 +1401,23 @@ public int uniquePathsIII(int[][] grid) {
         
     }
 
-
+// 35. Search Insert Position
+     public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if(target<nums[mid]){
+                high = mid - 1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
 
    public static void main(String[] args){
 
