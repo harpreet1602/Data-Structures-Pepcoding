2c6c65dcd0e6bade276f5df30c1796b6f812ae97
Harpreet Singh <harpreets1602@gmail.com>
2022-01-04 00:55:40 +0530

    update

--- Challenges/JanuaryChallenge2022/l001.java
+++ Challenges/JanuaryChallenge2022/l001.java
@@ -70,6 +70,27 @@ public class l001{
         ans += freqMap[30]*(freqMap[30]-1)/2;
         return ans;
     }
+        
+    
+//    997. Find the Town Judge 
+//    tc O(n) sc O(n) 
+//     any node with outdegree = 0 && indegree = n-1 will be the town judge
+    public int findJudge(int n, int[][] trust) {
+        int[] inwardDegree = new int[n+1];
+        int[] outwardDegree = new int[n+1];
+        for(int i=0;i<trust.length;i++){
+            int countOut = outwardDegree[trust[i][0]];
+            outwardDegree[trust[i][0]] = countOut + 1;
+            int countIn = inwardDegree[trust[i][1]];
+            inwardDegree[trust[i][1]] = countIn + 1;
+        }
+        
+        for(int i=1;i<=n;i++){
+            if(outwardDegree[i] == 0 && inwardDegree[i] == n-1){
+                return i;
+            }
+        }
+        return -1;
+    }
 
- 
 }
