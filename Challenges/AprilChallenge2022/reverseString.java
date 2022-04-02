df5bde5fb27855004e526254246d1b479c9fe946
Harpreet Singh <harpreets1602@gmail.com>
2022-04-03 02:15:49 +0530

    update

--- Challenges/AprilChallenge2022/reverseString.java
+++ Challenges/AprilChallenge2022/reverseString.java
@@ -15,4 +15,32 @@ class Solution {
             swap(s,low++,high--);
         }
     }
+
+
+    
+//     tc O(n) sc O(1)
+//     Just check the pallindrome and handle the deletion case by calling both the scenarios 
+//     i with j-1 or i+1 with j-1 and there on also check the same conditions from them onwards
+//     if you get true with any of the scenario then the ans is true otherwise false.
+    public boolean checkPal(String s,int i,int j){
+         while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false; 
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return checkPal(s,i,j-1) || checkPal(s,i+1,j); 
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
