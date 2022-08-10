c023db46a40526ec7cc9d4b385334ba3d8d43593
Harpreet Singh <harpreets1602@gmail.com>
2022-08-11 02:08:24 +0530

    update

---
+++ Challenges/AugustChallenge2022/sortedArrTobst.java
@@ -0,0 +1,20 @@
+public class sortedArrTobst{
+
+//     108. Convert Sorted Array to Binary Search Tree
+// tc O(n) sc O(n)=> recursive space I think     
+//     Middle element is the root at the current stage and similarly repeat the process for left and right children
+//     if at any case start becomes greater than end return null.
+public TreeNode sortedArrayToBST(int[] nums) {
+    return arrToBST(nums,0,nums.length-1);
+}
+private TreeNode arrToBST(int[] nums,int start,int end){
+    if(start>end){
+        return null;
+    }
+    int mid = (start+end)/2;
+    TreeNode root = new TreeNode(nums[mid]);
+    root.left = arrToBST(nums,start,mid-1);
+    root.right = arrToBST(nums,mid+1,end);
+    return root;
+}
+}
