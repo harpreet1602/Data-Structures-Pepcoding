17cbd7c48951d7e48a7407a6c757bae8124135bd
Harpreet Singh <harpreets1602@gmail.com>
2022-08-18 00:18:37 +0530

    update

---
+++ Challenges/AugustChallenge2022/uniqueMorseCodeWord.java
@@ -0,0 +1,24 @@
+public class uniqueMorseCodeWord {
+    
+    // 804. Unique Morse Code Words
+//     tc O(S), where S is the sum of the lengths of words in words. We iterate through each character of each word in words
+//     Space Complexity: O(S)
+//     Just make the string array for mapping and hashset to store unique strings and return hashset's size.
+public int uniqueMorseRepresentations(String[] words) {
+    String[] map = new String[]{
+        ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."
+    };
+    
+    HashSet<String> set = new HashSet<>();
+    
+    for(String word:words){
+        StringBuilder newStr = new StringBuilder();
+        for(char ch:word.toCharArray()){
+            int index = ch - 'a';
+            newStr.append(map[index]);
+        }
+        set.add(newStr.toString());
+    }
+    return set.size();
+}
+}
