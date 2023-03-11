f3a4116254e002a40360a1add7fdf11b4af2afd9
Harpreet Singh <harpreets1602@gmail.com>
2023-03-12 01:52:06 +0530

    update

---
+++ Love Babbar DSA 450/BST/sortedListToBST.java
@@ -0,0 +1,34 @@
+public class sortedListToBST{
+    
+// 109. Convert Sorted List to Binary Search Tree
+// tc O(nlogn) sc O(logn) recursive space.
+//     nlogn because n for finding the middle everytime and tree length goes till logn 
+//     Find the middle node through fast and slow pointers
+//     place end one node ahead so that start == end base case can be included for returning null => do dry run.
+//     start to slow on the left hand side and slow.next to end on right hand side.
+//     In the start also we are starting from head and end is null.
+    
+public TreeNode sortedListToBST(ListNode head) {
+    if(head == null){
+        return null;
+    }
+    return sortedListToBST(head,null);
+}
+private TreeNode sortedListToBST(ListNode start,ListNode end){
+    ListNode slow, fast;
+    slow = fast = start;
+    
+    if(start == end){
+        return null;
+    }
+    while(fast!=end && fast.next!=end){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    TreeNode root = new TreeNode(slow.val);
+    root.left = sortedListToBST(start,slow);
+    root.right = sortedListToBST(slow.next,end);
+    
+    return root;
+}
+}
