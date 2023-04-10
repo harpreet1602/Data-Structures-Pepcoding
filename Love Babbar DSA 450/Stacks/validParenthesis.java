e97595b40f5f66c3f967dcd0c0d9b7ac9168ca14
harpreet1602 <harpreets1602@gmail.com>
2023-04-10 16:50:29 +0530

    update

--- Love Babbar DSA 450/Stacks/validParenthesis.java
+++ Love Babbar DSA 450/Stacks/validParenthesis.java
@@ -34,3 +34,39 @@ public boolean isValid(String s) {
     return st.size()==0;
 }
 }
+
+class Solution {
+    //   20. Valid Parentheses
+    //   tc O(n) sc O(n)
+    //     just use stack add when opening bracket comes and remove and check when closing brackets come.
+    //     simply apply the logic.
+        public boolean isValid(String s) {
+            int n = s.length();
+            LinkedList<Character> st = new LinkedList<>();
+            
+            for(int i=0;i<n;i++){
+                char ch = s.charAt(i);
+                
+                if(ch == '(' || ch == '[' || ch == '{'){
+                    st.addFirst(ch);
+                }
+                else{
+                    if(st.size()==0){
+                        return false;
+                    }
+                    char top = st.removeFirst();
+                    
+                    if(ch == ')' && top != '('){
+                        return false;
+                    }
+                    if(ch == ']' && top != '['){
+                        return false;
+                    }
+                    if(ch == '}' && top != '{'){
+                        return false;
+                    }
+                }
+            }
+            return st.size() == 0;
+        }
+    }
